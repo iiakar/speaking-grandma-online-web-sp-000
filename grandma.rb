b9d561587482a1fa5dbfd2cab576f26af219ae17
Ibrahim  Akar <ibsakar@gmail.com>
2019-09-30 17:17:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,21 +13,21 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-def speak_to_grandma(string)
-  if string != string.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif string.upcase
-    return "NO, NOT SINCE 1938!"
-  end
-end 
-
 # def speak_to_grandma(string)
-# if string == "I LOVE YOU GRANDMA!"
-#   return "I LOVE YOU TOO PUMPKIN!"
-#   elsif string == string.upcase 
+#   if string != string.upcase
+#     return "HUH?! SPEAK UP, SONNY!"
+#   elsif string.upcase
 #     return "NO, NOT SINCE 1938!"
-#   elsif string != string.upcase
-#   return "HUH?! SPEAK UP, SONNY!"
-# else
-#   end 
-# end
+#   end
+# end 
+
+def speak_to_grandma(string)
+if string == "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN!"
+  elsif string == string.upcase 
+    return "NO, NOT SINCE 1938!"
+  elsif string != string.upcase
+  return "HUH?! SPEAK UP, SONNY!"
+else
+  end 
+end
